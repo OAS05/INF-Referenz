b5595bf9a1791537500b94d12c8303d4f828cb47
orlando <orlando.aurel.serba@gmail.com>
2025-04-10 06:29:53 +0200

    added first impression in index.html

---
+++ src/search.js
@@ -0,0 +1,15 @@
+function selection() {
+    let input, filter, ul, li, a, i;
+    input = document.getElementById("mySearch");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myMenu");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1 && input.value != "") {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
